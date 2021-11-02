56e82e35e2f77e7c63d0c7d167efe8153f72370b
Ben Chuan <benchuan0614@gmail.com>
2021-11-02 09:20:00 +0800

    調整麵包屑和分頁按鈕css

--- src/components/Yiling/AirQ/AirQuality.js
+++ src/components/Yiling/AirQ/AirQuality.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
-import "./airQuality.css";
+import React, { useState, useEffect } from 'react'
+import './airQuality.css'
 // import { GoogleKey } from "../StoresMap/Key";
-
 
 function AirQuality(props) {
   //獲取設備當前的位置 -> 取得經緯度 -> 經緯度轉換城市、區域名稱 -> 傳給Google Maps API
@@ -9,47 +8,46 @@ function AirQuality(props) {
   const [getGps, setGetGps] = useState({
     lat: 0,
     lng: 0,
-  });
+  })
 
   const [addr, setAddr] = useState({
-    city: "城市",
-    site: "地區",
-  });
+    city: '城市',
+    site: '地區',
+  })
 
-  const [aqi, setAqi] = useState(0);
-  const [aqiState, setAqiState] = useState("");
-  const [inputValue, setInputValue] = useState("");
+  const [aqi, setAqi] = useState(0)
+  const [aqiState, setAqiState] = useState('')
+  const [inputValue, setInputValue] = useState('')
 
   // //地址解析 (函式模組化)
   //https://maps.google.com/maps/api/geocode/json?latlng=24.9787789,121.4444248&language=zh-TW&sensor=true&key=${GoogleKey}
   //result_type 一種或多種地址類型的過濾器
   const fetchAddress = async () => {
     let addrStr = {
-      city: "",
-      site: "",
-    };
+      city: '',
+      site: '',
+    }
 
     if ((getGps.lat !== 0) & (getGps.lng !== 0)) {
-      const result = await fetch(
-        // `https://maps.google.com/maps/api/geocode/json?latlng=${getGps.lat},${getGps.lng}&result_type=street_address&language=zh-TW&sensor=true&key=${GoogleKey}`
-      ).then((res) => res.json());
-      let addrApi = result.results[0].address_components;
+      const result = await fetch().then((res) => res.json())
+      // `https://maps.google.com/maps/api/geocode/json?latlng=${getGps.lat},${getGps.lng}&result_type=street_address&language=zh-TW&sensor=true&key=${GoogleKey}`
+      let addrApi = result.results[0].address_components
 
       //解析城市區域
       addrApi.forEach((element) => {
         // console.log(element.types[0]);
-        if ("administrative_area_level_1" === element.types[0]) {
+        if ('administrative_area_level_1' === element.types[0]) {
           //console.log(element.long_name);
-          addrStr.city = element.long_name;
-        } else if ("administrative_area_level_3" === element.types[0]) {
+          addrStr.city = element.long_name
+        } else if ('administrative_area_level_3' === element.types[0]) {
           // console.log(element.long_name);
-          addrStr.site = element.long_name;
+          addrStr.site = element.long_name
         }
-      });
+      })
       //console.log(addrStr)
-      setAddr(addrStr);
+      setAddr(addrStr)
     }
-  };
+  }
 
   //導入空氣品質API Key:af2e94d5-755a-4e00-8fa5-bf85ffed4fa8
   //https://data.epa.gov.tw/api/v1/aqx_p_432?api_key=af2e94d5-755a-4e00-8fa5-bf85ffed4fa8
@@ -59,62 +57,62 @@ function AirQuality(props) {
       //   `https://data.epa.gov.tw/api/v1/aqx_p_432?api_key=af2e94d5-755a-4e00-8fa5-bf85ffed4fa8`
       // ).then((res) => res.json());
 
-      let result=require("./aqx_p_432.json");
-      let aqiApi = result.records;
+      let result = require('./aqx_p_432.json')
+      let aqiApi = result.records
       // console.log(aqiApi);
 
       aqiApi.forEach((e) => {
         //console.log(e);
         if (addr.site === e.SiteName) {
-          setAqi(e.AQI);
-          setAqiState(e.Status);
+          setAqi(e.AQI)
+          setAqiState(e.Status)
         }
-      });
+      })
 
       if (aqi === 0) {
         aqiApi.forEach((e) => {
           if (addr.city === e.County) {
-            setAqi(e.AQI);
-            setAqiState(e.Status);
+            setAqi(e.AQI)
+            setAqiState(e.Status)
           }
-        });
+        })
       }
     }
-  };
+  }
 
   const checkAddress = () => {
     //拷貝物件
-    const newAddr = JSON.parse(JSON.stringify(addr));
+    const newAddr = JSON.parse(JSON.stringify(addr))
 
-    if ("台" === newAddr.city.slice(0, 1)) {
-      newAddr.city = newAddr.city.replace("台", "臺");
+    if ('台' === newAddr.city.slice(0, 1)) {
+      newAddr.city = newAddr.city.replace('台', '臺')
       // console.log(newAddr.city);
     }
 
-    if (newAddr.site === "") {
-      newAddr.site = newAddr.city;
+    if (newAddr.site === '') {
+      newAddr.site = newAddr.city
     }
 
     if (newAddr.site.length > 2) {
-      newAddr.site = newAddr.site.slice(0, 2);
+      newAddr.site = newAddr.site.slice(0, 2)
       //console.log(newAddr.site.slice(0,2))
     }
     //物件跟物件的相等比較
     if (addr.city !== newAddr.city || addr.site !== newAddr.site) {
-      setAddr(newAddr);
+      setAddr(newAddr)
     }
-  };
+  }
 
   useEffect(() => {
-    fetchAddress();
+    fetchAddress()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [getGps]);
+  }, [getGps])
 
   useEffect(() => {
-    checkAddress();
-    fetchAQI();
+    checkAddress()
+    fetchAQI()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [addr]);
+  }, [addr])
 
   //定位 getG
   const getG = () => {
@@ -123,58 +121,57 @@ function AirQuality(props) {
         let pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
-        };
-        setGetGps(pos);
-      });
+        }
+        setGetGps(pos)
+      })
     } else {
-      alert("不支援");
+      alert('不支援')
     }
-  };
+  }
 
   //輸入值
   const inputEnter = (e) => {
-    setInputValue(e.target.value);
-  };
+    setInputValue(e.target.value)
+  }
 
   //搜尋
   const search = async () => {
     let addrStr = {
-      city: "",
-      site: "",
-    };
+      city: '',
+      site: '',
+    }
 
-    if (inputValue !== "") {
-      const result = await fetch(
-        // `https://maps.google.com/maps/api/geocode/json?&address=${inputValue}&result_type=street_address&language=zh-TW&sensor=true&key=${GoogleKey}`
-      ).then((res) => res.json());
-      let addrApi = result.results[0].address_components;
+    if (inputValue !== '') {
+      const result = await fetch().then((res) => res.json())
+      // `https://maps.google.com/maps/api/geocode/json?&address=${inputValue}&result_type=street_address&language=zh-TW&sensor=true&key=${GoogleKey}`
+      let addrApi = result.results[0].address_components
 
       //解析城市區域
       addrApi.forEach((element) => {
         // console.log(element.types[0]);
         if (
-          "administrative_area_level_1" === element.types[0] ||
-          "administrative_area_level_2" === element.types[0]
+          'administrative_area_level_1' === element.types[0] ||
+          'administrative_area_level_2' === element.types[0]
         ) {
           //console.log(element.long_name);
-          addrStr.city = element.long_name;
-        } else if ("administrative_area_level_3" === element.types[0]) {
+          addrStr.city = element.long_name
+        } else if ('administrative_area_level_3' === element.types[0]) {
           // console.log(element.long_name);
-          addrStr.site = element.long_name;
+          addrStr.site = element.long_name
         }
-      });
+      })
       //console.log(addrStr)
-      setAddr(addrStr);
-      setInputValue("");
+      setAddr(addrStr)
+      setInputValue('')
     }
-  };
+  }
 
   //鍵盤事件
   const keyUp = (e) => {
     if (e.keyCode === 13) {
-      search();
+      search()
     }
-  };
+  }
 
   return (
     <>
@@ -195,10 +192,10 @@ function AirQuality(props) {
             <input
               type="text"
               onChange={(e) => {
-                inputEnter(e);
+                inputEnter(e)
               }}
               onKeyUp={(e) => {
-                keyUp(e);
+                keyUp(e)
               }}
               value={inputValue}
               placeholder="    請輸入城市名稱"
@@ -208,7 +205,7 @@ function AirQuality(props) {
             <button
               className="button-7"
               onClick={() => {
-                getG();
+                getG()
               }}
             >
               GPS
@@ -216,7 +213,7 @@ function AirQuality(props) {
             <button
               className="button-7"
               onClick={() => {
-                search();
+                search()
               }}
             >
               搜尋
@@ -224,13 +221,13 @@ function AirQuality(props) {
           </div>
         </div>
         <div className="stateAllGood">
-          <p className="stateGood">{aqiState === "" ? "" : aqiState}</p>
-          <p className="stateNumber">{aqi === 0 ? "AQI" : aqi}</p>
+          <p className="stateGood">{aqiState === '' ? '' : aqiState}</p>
+          <p className="stateNumber">{aqi === 0 ? 'AQI' : aqi}</p>
           <p className="stateGood">AQI</p>
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default AirQuality;
+export default AirQuality
--- src/pages/Yiling/PortiaAllPage/PortiaAllPage.js
+++ src/pages/Yiling/PortiaAllPage/PortiaAllPage.js
@@ -1,45 +1,39 @@
 /* eslint-disable react/jsx-pascal-case */
-import React from "react";
-import "./PortiaAllPage.css";
+import React from 'react'
+import './PortiaAllPage.css'
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-
-
-import HomePage from "../Home/HomePage"; //引入首頁
-import StoresMap from "../StoresMap/StoresMap"; //門市資訊
-import HealthEnCyc from "../HealthEnCyc/HealthEnCyc"; //保健百科頁面
-import HealthEnIndex from "../HealthEnIndex/HealthEnIndex"; //保健百科頁面的內頁
-import Select_shop from "../Select_Shop/Select_shop"; //處方籤預約領藥
-
-
+import HomePage from '../Home/HomePage' //引入首頁
+import StoresMap from '../StoresMap/StoresMap' //門市資訊
+import HealthEnCyc from '../HealthEnCyc/HealthEnCyc' //保健百科頁面
+import HealthEnIndex from '../HealthEnIndex/HealthEnIndex' //保健百科頁面的內頁
+import Select_shop from '../Select_Shop/Select_shop' //處方籤預約領藥
 
 function PortiaAllPage(props) {
-  const {setCloseStore}=props
+  const { setCloseStore } = props
   return (
-    
-      <>
-        {/* 主要內容 */}
-        <Switch>
-          <Route path="/HomePage">
-            <HomePage />
-          </Route>
-          <Route path="/StoresMap">
-            <StoresMap />
-          </Route>
-          <Route path="/HealthEnCyc">
-            <HealthEnCyc />
-          </Route>
-          <Route path="/HealthEnIndex/:Id">
-            <HealthEnIndex />
-          </Route>
-          <Route path="/Select_shop">
-            <Select_shop setCloseStore={setCloseStore} />
-          </Route>
-        </Switch>
-      </>
-    
-  );
+    <>
+      {/* 主要內容 */}
+      <Switch>
+        <Route path="/HomePage">
+          <HomePage />
+        </Route>
+        <Route path="/StoresMap">
+          <StoresMap />
+        </Route>
+        <Route path="/HealthEnCyc">
+          <HealthEnCyc />
+        </Route>
+        <Route path="/HealthEnIndex/:Id">
+          <HealthEnIndex />
+        </Route>
+        <Route path="/Select_shop">
+          <Select_shop setCloseStore={setCloseStore} />
+        </Route>
+      </Switch>
+    </>
+  )
 }
 
-export default PortiaAllPage;
+export default PortiaAllPage
